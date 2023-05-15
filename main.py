ce27289592516eda5f6df9ff983952d521f6a555
zernov.g.a <zernov.g.a@gmail.com>
2023-05-15 09:49:37 +0700

    new file:   ObjLoader.py 	new file:   TextureLoader.py 	new file:   __pycache__/ObjLoader.cpython-310.pyc 	new file:   __pycache__/TextureLoader.cpython-310.pyc 	new file:   __pycache__/camera.cpython-310.pyc 	new file:   camera.py 	modified:   main.py 	new file:   meshes/chibi.obj 	new file:   meshes/chibi.png 	new file:   meshes/cube.jpg 	new file:   meshes/cube.obj 	new file:   meshes/floor.jpg 	new file:   meshes/floor.obj 	new file:   meshes/monkey.jpg 	new file:   meshes/monkey.obj 	new file:   meshes/monkey_smooth.obj 	modified:   requirements.txt 	new file:   textures/brick.jpg 	new file:   textures/cat.png 	new file:   textures/crate.jpg 	new file:   textures/metal.jpg 	new file:   textures/smiley.png

--- main.py
+++ main.py
@@ -1,103 +1,249 @@
-import pygame as pg
-from pygame.locals import *
+import os
+os.environ['SDL_VIDEO_WINDOW_POS'] = '400,200'
 
+import pygame
 from OpenGL.GL import *
-from OpenGL.GLU import *
+from OpenGL.GL.shaders import compileProgram, compileShader
+import pyrr
+from TextureLoader import load_texture_pygame
+from ObjLoader import ObjLoader
 
 from camera import Camera
 
+# CAMERA settings
 cam = Camera()
-
-cubeVertices = ((1,1,1),(1,1,-1),(1,-1,-1),(1,-1,1),(-1,1,1),(-1,-1,-1),(-1,-1,1),(-1,1,-1))
-cubeEdges = ((0,1),(0,3),(0,4),(1,2),(1,7),(2,5),(2,3),(3,6),(4,6),(4,7),(5,6),(5,7))
-cubeQuads = ((0,3,6,4),(2,5,6,3),(1,2,5,7),(1,0,4,7),(7,4,6,5),(2,3,0,1))
-
-def wireCube():
-    glBegin(GL_LINES)
-    for cubeEdge in cubeEdges:
-        for cubeVertex in cubeEdge:
-            glVertex3fv(cubeVertices[cubeVertex])
-    glEnd()
-
-def solidCube():
-    glBegin(GL_QUADS)
-    for cubeQuad in cubeQuads:
-        for cubeVertex in cubeQuad:
-            glVertex3fv(cubeVertices[cubeVertex])
-    glEnd()
-
-def move_camera():
-    pressed_keys = pg.key.get_pressed()
-    x, y, z = 0.0, 0.0, 0.0
-    if pressed_keys[K_w]:
-        y += 0.1
-    if pressed_keys[K_s]:
-        y -= 0.1
-    if pressed_keys[K_d]:
-        x += 0.1
-    if pressed_keys[K_a]:
-        x -= 0.1
-    if pressed_keys[K_e]:
-        z += 0.1
-    if pressed_keys[K_q]:
-        z -= 0.1
-    glTranslatef(x, y, z)
-
-    if pressed_keys[K_KP_8]:
-        glRotatef(0.5, 1, 0, 0)
-    if pressed_keys[K_KP_2]:
-        glRotatef(-0.5, 1, 0, 0)
-    if pressed_keys[K_KP_4]:
-        glRotatef(-0.5, 0, 1, 0)
-    if pressed_keys[K_KP_6]:
-        glRotatef(0.5, 0, 1, 0)
-    if pressed_keys[K_KP_7]:
-        glRotatef(-0.5, 0, 0, 1)
-    if pressed_keys[K_KP_9]:
-        glRotatef(0.5, 0, 0, 1)
-
-    if pressed_keys[K_ESCAPE]:
-        pg.quit()
-        quit()
-
-def main():
-    pg.init()
-    display = (1280, 720)
-    pg.display.set_mode(display, DOUBLEBUF|OPENGL)
-
-    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
-
-    glTranslatef(0.0, 0.0, -10.0)
+WIDTH, HEIGHT = 1280, 720
+lastX, lastY = WIDTH / 2, HEIGHT / 2
+first_mouse = True
 
 
-    while True:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                pg.quit()
-                quit()
+vertex_src = """
+# version 300 es
 
-        # glRotatef(1, 1, 1, 1)
-        # move_camera()
+precision mediump float;
 
-        keys_pressed = pg.key.get_pressed()
-            if keys_pressed[pg.K_a]:
-                cam.process_keyboard("LEFT", 0.08)
-            if keys_pressed[pg.K_d]:
-                cam.process_keyboard("RIGHT", 0.08)
-            if keys_pressed[pg.K_w]:
-                cam.process_keyboard("FORWARD", 0.08)
-            if keys_pressed[pg.K_s]:
-                cam.process_keyboard("BACKWARD", 0.08)
+layout(location = 0) in vec3 a_position;
+layout(location = 1) in vec2 a_texture;
+layout(location = 2) in vec3 a_normal;
 
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+uniform mat4 model;
+uniform mat4 projection;
+uniform mat4 view;
 
-        view = cam.get_view_matrix()
-        glUniformMatrix4fv(view_loc, 1, GL_FALSE, view)
-        
-        # solidCube()
-        wireCube()
-        pg.display.flip()
-        pg.time.wait(10)
+out vec2 v_texture;
 
-if __name__ == "__main__":
-    main()
+void main()
+{
+    gl_Position = projection * view * model * vec4(a_position, 1.0);
+    v_texture = a_texture;
+}
+"""
+
+fragment_src = """
+# version 300 es
+
+precision mediump float;
+
+in vec2 v_texture;
+
+out vec4 out_color;
+
+uniform sampler2D s_texture;
+
+void main()
+{
+    out_color = texture(s_texture, v_texture);
+}
+"""
+
+
+def mouse_look(xpos, ypos):
+    global first_mouse, lastX, lastY
+
+    if first_mouse:
+        lastX = xpos
+        lastY = ypos
+        first_mouse = False
+
+    xoffset = xpos - lastX
+    yoffset = lastY - ypos
+
+    lastX = xpos
+    lastY = ypos
+
+    cam.process_mouse_movement(xoffset, yoffset)
+
+
+pygame.init()
+pygame.display.set_mode((WIDTH, HEIGHT), pygame.OPENGL | pygame.DOUBLEBUF | pygame.RESIZABLE) # |pygame.FULLSCREEN
+pygame.mouse.set_visible(False)
+pygame.event.set_grab(True)
+
+# load here the 3d meshes
+cube_indices, cube_buffer = ObjLoader.load_model("meshes/cube.obj", False)
+monkey_indices, monkey_buffer = ObjLoader.load_model("meshes/monkey.obj")
+floor_indices, floor_buffer = ObjLoader.load_model("meshes/floor.obj")
+
+shader = compileProgram(compileShader(vertex_src, GL_VERTEX_SHADER), compileShader(fragment_src, GL_FRAGMENT_SHADER))
+
+# VAO and VBO
+VAO = glGenVertexArrays(3)
+VBO = glGenBuffers(3)
+EBO = glGenBuffers(1)
+
+# cube VAO
+glBindVertexArray(VAO[0])
+# cube Vertex Buffer Object
+glBindBuffer(GL_ARRAY_BUFFER, VBO[0])
+glBufferData(GL_ARRAY_BUFFER, cube_buffer.nbytes, cube_buffer, GL_STATIC_DRAW)
+
+glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO)
+glBufferData(GL_ELEMENT_ARRAY_BUFFER, cube_indices.nbytes, cube_indices, GL_STATIC_DRAW)
+
+# cube vertices
+glEnableVertexAttribArray(0)
+glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, cube_buffer.itemsize * 8, ctypes.c_void_p(0))
+# cube textures
+glEnableVertexAttribArray(1)
+glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, cube_buffer.itemsize * 8, ctypes.c_void_p(12))
+# cube normals
+glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, cube_buffer.itemsize * 8, ctypes.c_void_p(20))
+glEnableVertexAttribArray(2)
+
+# monkey VAO
+glBindVertexArray(VAO[1])
+# monkey Vertex Buffer Object
+glBindBuffer(GL_ARRAY_BUFFER, VBO[1])
+glBufferData(GL_ARRAY_BUFFER, monkey_buffer.nbytes, monkey_buffer, GL_STATIC_DRAW)
+
+# monkey vertices
+glEnableVertexAttribArray(0)
+glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, monkey_buffer.itemsize * 8, ctypes.c_void_p(0))
+# monkey textures
+glEnableVertexAttribArray(1)
+glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, monkey_buffer.itemsize * 8, ctypes.c_void_p(12))
+# monkey normals
+glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, monkey_buffer.itemsize * 8, ctypes.c_void_p(20))
+glEnableVertexAttribArray(2)
+
+# floor VAO
+glBindVertexArray(VAO[2])
+# floor Vertex Buffer Object
+glBindBuffer(GL_ARRAY_BUFFER, VBO[2])
+glBufferData(GL_ARRAY_BUFFER, floor_buffer.nbytes, floor_buffer, GL_STATIC_DRAW)
+
+# floor vertices
+glEnableVertexAttribArray(0)
+glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, floor_buffer.itemsize * 8, ctypes.c_void_p(0))
+# floor textures
+glEnableVertexAttribArray(1)
+glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, floor_buffer.itemsize * 8, ctypes.c_void_p(12))
+# floor normals
+glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, floor_buffer.itemsize * 8, ctypes.c_void_p(20))
+glEnableVertexAttribArray(2)
+
+glEnable(GL_BLEND)
+glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+
+textures = glGenTextures(3)
+load_texture_pygame("meshes/cube.jpg", textures[0])
+load_texture_pygame("meshes/monkey.jpg", textures[1])
+load_texture_pygame("meshes/floor.jpg", textures[2])
+
+glUseProgram(shader)
+glClearColor(0, 0.1, 0.1, 1)
+glEnable(GL_DEPTH_TEST)
+glEnable(GL_BLEND)
+glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+
+projection = pyrr.matrix44.create_perspective_projection_matrix(45, 1280 / 720, 0.1, 100)
+cube_pos = pyrr.matrix44.create_from_translation(pyrr.Vector3([6, 4, 0]))
+monkey_pos = pyrr.matrix44.create_from_translation(pyrr.Vector3([-4, 4, -4]))
+floor_pos = pyrr.matrix44.create_from_translation(pyrr.Vector3([0, 0, 0]))
+
+model_loc = glGetUniformLocation(shader, "model")
+proj_loc = glGetUniformLocation(shader, "projection")
+view_loc = glGetUniformLocation(shader, "view")
+
+glUniformMatrix4fv(proj_loc, 1, GL_FALSE, projection)
+
+running = True
+
+font = pygame.font.SysFont('Ubuntu', 64)
+
+def drawText(x, y, text):                                                
+    textSurface = font.render(text, True, 
+                              (255, 255, 255), (255, 0, 0))
+    textData = pygame.image.tostring(textSurface, "RGB", True)
+    glGenTextures( 1, textData)
+    glBindTexture( GL_TEXTURE_2D, textData )
+    glWindowPos2d(x, y)
+    glDrawPixels(textSurface.get_width(), textSurface.get_height(), 
+                 GL_RGB, GL_UNSIGNED_BYTE, textData)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif  event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            running = False
+
+        if event.type == pygame.VIDEORESIZE:
+            glViewport(0, 0, event.w, event.h)
+            projection = pyrr.matrix44.create_perspective_projection_matrix(45, event.w / event.h, 0.1, 100)
+            glUniformMatrix4fv(proj_loc, 1, GL_FALSE, projection)
+
+    keys_pressed = pygame.key.get_pressed()
+    if keys_pressed[pygame.K_a]:
+        cam.process_keyboard("LEFT", 0.08)
+    if keys_pressed[pygame.K_d]:
+        cam.process_keyboard("RIGHT", 0.08)
+    if keys_pressed[pygame.K_w]:
+        cam.process_keyboard("FORWARD", 0.08)
+    if keys_pressed[pygame.K_s]:
+        cam.process_keyboard("BACKWARD", 0.08)
+
+
+    mouse_pos = pygame.mouse.get_pos()
+    mouse_look(mouse_pos[0], mouse_pos[1])
+
+    # to been able to look around 360 degrees, still not perfect
+    if mouse_pos[0] <= 0:
+        pygame.mouse.set_pos((1279, mouse_pos[1]))
+    elif mouse_pos[0] >= 1279:
+        pygame.mouse.set_pos((0, mouse_pos[1]))
+
+
+    ct = pygame.time.get_ticks() / 1000
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+    view = cam.get_view_matrix()
+    glUniformMatrix4fv(view_loc, 1, GL_FALSE, view)
+
+    rot_y = pyrr.Matrix44.from_y_rotation(0.8 * ct)
+    model = pyrr.matrix44.multiply(rot_y, cube_pos)
+
+    # draw the cube
+    glBindVertexArray(VAO[0])
+    glBindTexture(GL_TEXTURE_2D, textures[0])
+    glUniformMatrix4fv(model_loc, 1, GL_FALSE, model)
+    glDrawElements(GL_TRIANGLES, len(cube_indices), GL_UNSIGNED_INT, None)
+
+    # draw the monkey
+    glBindVertexArray(VAO[1])
+    glBindTexture(GL_TEXTURE_2D, textures[1])
+    glUniformMatrix4fv(model_loc, 1, GL_FALSE, monkey_pos)
+    glDrawArrays(GL_TRIANGLES, 0, len(monkey_indices))
+
+    # draw the floor
+    glBindVertexArray(VAO[2])
+    glBindTexture(GL_TEXTURE_2D, textures[2])
+    glUniformMatrix4fv(model_loc, 1, GL_FALSE, floor_pos)
+    glDrawArrays(GL_TRIANGLES, 0, len(floor_indices))
+
+    drawText(480, 120, "test")
+
+    pygame.display.flip()
+
+pygame.quit()
